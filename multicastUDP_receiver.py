5eace3862e13a06eb8e70c560f72b717918134c3
Edgar W <techniccontroller@gmail.com>
2022-02-26 00:30:13 +0100

    add network interface address

--- multicastUDP_receiver.py
+++ multicastUDP_receiver.py
@@ -2,6 +2,9 @@ import socket
 import struct
 import sys
 
+# ip address of network interface
+MCAST_IF_IP = '192.168.0.4'
+
 multicast_group = '230.120.10.2'
 server_address = ('', 8123)
 
@@ -16,8 +19,10 @@ print("Start")
 # Tell the operating system to add the socket to the multicast group
 # on all interfaces.
 group = socket.inet_aton(multicast_group)
-mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+mreq = struct.pack('4s4s', group, socket.inet_aton(MCAST_IF_IP))
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+print("Ready")
 
 # Receive/respond loop
 while True:
