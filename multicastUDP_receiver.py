721ea0c16aaf5a91988a73d157308841f788ab70
Edgar W <techniccontroller@gmail.com>
2023-05-07 14:02:56 +0200

    add logging to file

--- multicastUDP_receiver.py
+++ multicastUDP_receiver.py
@@ -1,9 +1,10 @@
 import socket
 import struct
 import sys
+from datetime import datetime
 
 # ip address of network interface
-MCAST_IF_IP = '192.168.0.4'
+MCAST_IF_IP = '192.168.0.9'
 
 multicast_group = '230.120.10.2'
 server_address = ('', 8123)
@@ -23,9 +24,22 @@ mreq = struct.pack('4s4s', group, socket.inet_aton(MCAST_IF_IP))
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 print("Ready")
+saveCounter = 0
 
 # Receive/respond loop
 while True:
     data, address = sock.recvfrom(1024)
-    print(address, ": ", data.decode("utf-8").strip())
+    data_str = data.decode("utf-8").strip()
+    print(address, ": ", data_str)
+
+    if "NTP-Update not successful" in data_str:
+        saveCounter = 3
+        
+    if saveCounter > 0:
+        f = open("log.txt",'a')
+        f.write(datetime.now().strftime('%b-%d-%Y_%H%M') + ": ")
+        f.write(data_str)
+        f.write("\n")
+        f.close()
+        saveCounter -= 1
 
